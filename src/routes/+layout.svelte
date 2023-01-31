e8a4c010e8b015f09afdb469856811aad28c2a5a
Napalmhammer <william.orourke@gmail.com>
2023-01-30 22:23:45 -0500

    Added some components, adjusted some styling, played around with grids

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,34 @@
+<TopAppBar bind:this={topAppBar} variant="standard">
+    <Row>
+        <Section>
+        <IconButton class="material-icons">menu</IconButton>
+        <Title>Squilliam's WoT Stats</Title>
+        </Section>
+        <Section align="end" toolbar>
+        <IconButton class="material-icons" aria-label="Download"
+            >file_download</IconButton
+        >
+        <IconButton class="material-icons" aria-label="Print this page"
+            >print</IconButton
+        >
+        <IconButton class="material-icons" aria-label="Bookmark this page"
+            >bookmark</IconButton
+        >
+        </Section>
+    </Row>
+</TopAppBar>
+<AutoAdjust {topAppBar}>
+    <slot />
+</AutoAdjust>
+
+<script lang="ts">
+    import TopAppBar, {
+        Row,
+        Section,
+        Title,
+        AutoAdjust,
+    } from '@smui/top-app-bar';
+    import IconButton from '@smui/icon-button';
+
+    let topAppBar: TopAppBar;
+</script>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import Button from '@smui/button';
+    import Textfield from '@smui/textfield';
+    import Icon from '@smui/textfield/icon';
+    import HelperText from '@smui/textfield/helper-text';
+    import Select, { Option } from '@smui/select';
+    import LayoutGrid, { Cell } from '@smui/layout-grid';
+
+    let valueA = '';
+    let regions = ['NA Server', 'EU Server', 'Russian Server', 'SEA Server'];
+    let selectedRegion = 'NA Server';
+</script>
+
+<LayoutGrid
+  align="middle"
+  fixedColumnWidth
+>
+    <Cell span={4}>
+        <div>
+            <Textfield variant="outlined" bind:value={valueA} label="Tanker name">
+            </Textfield>
+        </div>
+    </Cell>
+    <Cell span={4}>
+        <div>
+            <Select variant="outlined" bind:value={selectedRegion} label="Region">
+                {#each regions as region}
+                    <Option value={region}>{region}</Option>
+                {/each}
+            </Select>
+        </div>
+    </Cell>
+    <Cell span={4}>
+        <div>
+            <Button variant="raised">Show Stats</Button>
+        </div>
+    </Cell>
+</LayoutGrid>
